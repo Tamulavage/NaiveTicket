39b5814462a292ac165154333119e6e481ce037f
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro.local>
2019-02-06 10:08:36 -0500

    ran book updates

--- TicketMachine.java
+++ TicketMachine.java
@@ -9,7 +9,7 @@
  * @author David J. Barnes and Michael Kolling
  * @version 2008.03.30
  */
-public class TicketMachine
+class TicketMachine
 {
     // The price of a ticket from this machine.
     private int price;
@@ -17,19 +17,34 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    
+    int status;
 
     /**
      * Create a machine that issues tickets of the given price.
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
      */
-    public TicketMachine(int ticketCost)
+    public TicketMachine()
     {
-        price = ticketCost;
+        price = 1000;
+        balance = 0;
+        total = 0;
+    }
+        public TicketMachine(int newTicketPrice)
+    {
+        price = newTicketPrice;
         balance = 0;
         total = 0;
     }
 
+    public void setPrice(int ticketCost) {
+        price = ticketCost;
+    }
+        public void empty() {
+        total = 0;
+    }
+    
     /**
      * Return the price of a ticket.
      */
@@ -42,17 +57,39 @@ public class TicketMachine
      * Return the amount of money already inserted for the
      * next ticket.
      */
-    public int getBalance()
+    public int getAmount()
     {
         return balance;
     }
-
+    
+    public int  getTotal()
+    {
+        return total;
+    }
+    
+    public void  prompt()
+    {
+        System.out.println("Please insert the correct amount of money.");
+    }
+    public void  showPrice()
+    {
+        System.out.println("the price of a ticket is " + price + " cents.");
+    }
     /**
      * Receive an amount of money in cents from a customer.
      */
     public void insertMoney(int amount)
     {
         balance = balance + amount;
+    }
+    
+       public int emptyMachine()
+    {
+        int hold;
+        hold=total; // for return value
+        total=0; //reset to 0
+        
+        return hold;
     }
 
     /**
@@ -62,9 +99,12 @@ public class TicketMachine
      */
     public void printTicket()
     {
-        // Simulate the printing of a ticket.
+        int amountLeftToPay = 0;
+        amountLeftToPay = balance - price;
+
+        if(amountLeftToPay >= 0){        // Simulate the printing of a ticket.
         System.out.println("##################");
-        System.out.println("# The BlueJ Line");
+        System.out.println("# The BlueJ Line ticket");
         System.out.println("# Ticket");
         System.out.println("# " + price + " cents.");
         System.out.println("##################");
@@ -72,7 +112,10 @@ public class TicketMachine
 
         // Update the total collected with the balance.
         total = total + balance;
-        // Clear the balance.
-        balance = 0;
+        balance = balance -price;
     }
+    else {
+        System.out.println("You must insert at least:  " + (price-balance) + " more cents." );
+    }
+}
 }
